49a83b84598f9af4c4c7a779cd23cd20725bd623
mac <mac@macs-Mac-Pro.local>
2022-06-08 16:37:19 +0530

    init

---
+++ lib/AppConstant.dart
@@ -0,0 +1,5 @@
+List<String> dropDownList = ["TEXT", "PHONE"];
+
+String imageFolder = "Output";
+// String appDirectory="G:\\Program Files\\QR Code Generator";
+String appDirectory = "Users\\mac\\Downloads";
